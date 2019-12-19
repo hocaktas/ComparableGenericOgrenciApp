0c816359b2f8d4506a7662e8d5d4fd0be82564fd
Kemalettin Aktaş <kemalettin.aktas@gazi.edu.tr>
2019-12-19 10:35:23 +0300

    Proje dosyası ekle.

---
+++ ComparableGenericOgrenciApp/Ogrenci.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparableGenericOgrenciApp
+{
+    class Ogrenci : IComparable
+    {
+
+        public int No { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public DateTime DogumTarihi { get; set; }
+        public int Yas
+        {
+            get
+            {
+                var yas = DateTime.Now - this.DogumTarihi;
+                return (int)yas.TotalDays / 365;
+            }
+
+        }
+        public Ogrenci(int no, string ad,string soyad, DateTime dogumTarihi)
+        {
+            this.No = no;
+            this.Ad = ad;
+            this.Soyad = soyad;
+            this.DogumTarihi = dogumTarihi;
+        }
+      
+        public override string ToString()
+        {
+            return string.Format("Öğrenci No:{0}\n\tAdı:{1}\n\tSoyadı:{2}\n\tYaşı:{3}",this.No,this.Ad,this.Soyad,this.Yas);
+        }
+
+        public int CompareTo(object obj)
+        {
+            Ogrenci o = (Ogrenci)obj;
+            return No.CompareTo(o.No);
+          
+        }
+    }
+}
---
+++ ComparableGenericOgrenciApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparableGenericOgrenciApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Ogrenci> ogrenciler = new List<Ogrenci>();
+            ogrenciler.Add(new Ogrenci(27, "Ali", "Kara", new DateTime(2001, 10, 30)));
+            ogrenciler.Add(new Ogrenci(100, "Kemalettin", "Aktaş", new DateTime(1987, 5, 18)));
+            ogrenciler.Add(new Ogrenci(101, "Fatma", "Aktaş", new DateTime(1991, 2, 20)));
+            ogrenciler.Add(new Ogrenci(199, "Muhammet", "Kalkan", new DateTime(1950, 5, 5)));
+
+            foreach (Ogrenci item in ogrenciler)
+            {
+                Console.WriteLine(item);
+            }
+            ogrenciler.Sort();
+            Console.WriteLine("------------------------------------------------------");
+            foreach (Ogrenci item in ogrenciler)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
